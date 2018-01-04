ed12797fc92c6c21981b5c6f700238aaf0c8b9b9
Teerasej <teerasej@gmail.com>
2018-01-04 14:25:14 +0700

    clear for start

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,22 +7,8 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  contactArray = [
-    { name:'แจ้งเหตุด่วนเหตุร้าย', phone:'191' }
-    ,{ name:'แจ้งเหตุเพลิงไหม้', phone:'199' }
-    ,{ name:'หน่วยแพทย์กู้ชีพ กทม.', phone:'1554' }
-    ,{ name:'ร่วมด้วยช่วยกัน', phone:'1677' }
-    ,{ name:'จส. 100', phone:'1137' }
-  ]
-
-
   constructor(public navCtrl: NavController) {
 
-  }
-
-  call(contact){
-    // alert(contact.name + ' ' + contact.phone);
-    window.open('tel:' + contact.phone);
   }
 
 }
